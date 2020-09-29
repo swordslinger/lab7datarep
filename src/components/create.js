2f0f62af74ca6519029b0632160da7c8709cd1d3
mkenirons <martin.kenirons@gmail.com>
2020-09-29 12:22:01 +0100

    lab complete

---
+++ src/components/create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Create extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>This is my Create Component.</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/movies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {MovieItem} from './movieItem';
+
+export class Movies extends React.Component{
+
+    render(){
+        return this.props.movies.map( (movie)=>{
+            return <MovieItem movie={movie}></MovieItem>
+        })
+    }
+}
